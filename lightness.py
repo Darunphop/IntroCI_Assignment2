d910d8dd53c597cd812c73360999469c89ab44ea
ICQCQ <darunphop.p@gmail.com>
2018-12-14 17:53:38 +0700

    changed back

--- lightness.py
+++ lightness.py
@@ -7,14 +7,10 @@ class Lightness:
         self.fHowBright = np.vectorize(self.howBright)
         self.fHowDark = np.vectorize(self.howDark)
 
-    def howBright(self, _lumen=[]):
-        if len(_lumen) == 0:
-            _lumen = self.lumen
+    def howBright(self, _lumen):
         return max(0.0, min(1.0, (_lumen - 400.0)/(1000.0 - 400.0)))
 
     def howDark(self, _lumen):
-        if len(_lumen) == 0:
-            _lumen = self.lumen
         return max(0.0,min(1.0, 1.0 - (_lumen - 400.0)/(600.0 - 400.0)))
 
 if __name__ =='__main__':
@@ -23,6 +19,6 @@ if __name__ =='__main__':
     l = Lightness(x)
     # print(list(x))
     y = np.asarray([555,666,789])
-    print(l.fHowBright())
+    print(l.fHowBright(l.lumen))
     print(l.fHowBright(y))
-    print(l.fHowDark())
+    print(l.fHowDark(y))
